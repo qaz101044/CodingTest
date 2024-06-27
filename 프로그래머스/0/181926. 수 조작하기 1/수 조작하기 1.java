dcb26c9b08820d2cfafd4ef1b0a9f228f5dd628e
Kim Hong Hee <105047982+qaz101044@users.noreply.github.com>
2024-06-27 21:55:35 +0900

    [level 0] Title: 수 조작하기 1, Time: 20.83 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,21 +1,27 @@
 class Solution {
     public int solution(int n, String control) {
         int answer = n;
-        String[] controlList = control.split("");
-        for(int i = 0; i<controlList.length; i++){
-            if(controlList[i].equals("w")){
-                answer+=1;
+        String[] control_arr = control.split("");
+        
+        for (int i=0; i < control_arr.length; i++) {
+            
+            if (control_arr[i].equals("w")) {
+                answer += 1;
             }
-            else if(controlList[i].equals("s")){
-                answer-=1;
+            else if (control_arr[i].equals("s")) {
+                answer -= 1;
             }
-            else if(controlList[i].equals("d")){
-                answer+=10;
+            
+            else if (control_arr[i].equals("d")) {
+                answer += 10;
             }
-            else if(controlList[i].equals("a")){
-                answer-=10;
+            
+            else if (control_arr[i].equals("a")) {
+                answer -= 10;
             }
         }
+        
+        
         return answer;
     }
 }
