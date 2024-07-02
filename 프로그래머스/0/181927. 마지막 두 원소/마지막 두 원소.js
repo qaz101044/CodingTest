21a5b184270c6295ead45afc4329a12bec569a7b
Kim Hong Hee <105047982+qaz101044@users.noreply.github.com>
2024-07-02 14:52:52 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let last = num_list[num_list.length-1];
+    let last_2 = num_list[num_list.length-2];
+    
+    if(last > last_2) {
+        num_list.push(last - last_2)
+    } 
+    
+    else {
+        num_list.push(last * 2)
+    }
+    
+    return num_list;
+}
